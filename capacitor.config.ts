84b44ee30bbe22783b651ab6cf5623a358a85591
Damir Filaretov <damir.filaretov@gmail.com>
2026-02-24 09:25:16 -0600

    some changes in google auth

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.damir.iskilog',
+  appName: 'iSkiLog',
+  webDir: 'dist'
+};
+
+export default config;
